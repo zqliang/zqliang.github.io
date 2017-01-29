5e343cf76d0e5422e119ad405e436c8a4dec5267
zqliang <liangjessy@gmail.com>
2017-01-28 23:06:59 -0500

    responsive page

---
+++ assets/scripts/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+  //fade images on mouse hover
+  $(".logos").hover(function(){
+        $(this).animate({
+        opacity: '0.7',
+        padding: '5px'
+    }, 100);
+        }, function(){
+        $(this).animate({
+        opacity: '1',
+        padding: '5px'
+
+    });
+
+    });
+ 
+  // Add smooth scrolling to all links in navbar + footer link
+  $("a, .navbar a, footer a[href='#home'], section a[href='#ct']").on('click', function(event) {
+
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, function(){
+   
+        window.location.hash = hash;
+
+      });
+    }
+
+  });
+  
+  $(window).scroll(function() {
+    //change nav bar color on scroll/click
+    if ($(document).scrollTop() > 600){
+      $('nav').css('background-color', '#c2b6b8');
+    }
+    else{
+      $('nav').css('background-color', '#b2b2b2');
+    }
+
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+
+  });
+})
